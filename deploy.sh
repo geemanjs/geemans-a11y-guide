c8949860287fd7802355783029d893e081a7e971
Geeman <geemanjs@gmail.com>
2017-04-29 00:01:46 +0100

    Remove need for compile file

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ./compile.sh
+  yarn build
+  cp -R build out
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -25,28 +26,27 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into build/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO build
-cd build
+git clone $REPO out
+cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf build/**/* || exit 0
+rm -rf out/**/* || exit 0
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
-cd build
-ls -al
+cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-#if [ -z `git diff --exit-code` ]; then
-#    echo "No changes to the output on this push; exiting."
-#    exit 0
-#fi
+# If there are no changes (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the docs on this push; exiting."
+    exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
